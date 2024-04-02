dbc98e98175659f057555b36bb9702e1f48b8d5c
Roy <sanga1224@naver.com>
2024-04-02 09:41:07 +0900

    firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,10 @@
+import 'dart:convert';
+
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:mobile_ebiz/firebase_options.dart';
 import 'package:mobile_ebiz/models/status_msg.dart';
 import 'package:mobile_ebiz/screens/main_screen.dart';
@@ -7,9 +12,79 @@ import 'package:mobile_ebiz/screens/splash_screen.dart';
 import 'package:mobile_ebiz/services/api_login.dart';
 import 'package:mobile_ebiz/themes/theme.dart';
 import 'package:mobile_ebiz/themes/theme_provider.dart';
+import 'package:mobile_ebiz/widgets/message/message.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:easy_localization/easy_localization.dart';
+
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await setupFlutterNotifications();
+  showFlutterNotification(message);
+}
+
+late AndroidNotificationChannel channel;
+bool isFlutterLocalNotificationsInitialized = false;
+
+Future<void> setupFlutterNotifications() async {
+  if (isFlutterLocalNotificationsInitialized) {
+    return;
+  }
+  channel = const AndroidNotificationChannel(
+    'high_importance_channel', // id
+    'High Importance Notifications', // title
+    description:
+        'This channel is used for important notifications.', // description
+    importance: Importance.high,
+  );
+
+  flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+
+  /// Create an Android Notification Channel.
+  ///
+  /// We use this channel in the `AndroidManifest.xml` file to override the
+  /// default FCM channel to enable heads up notifications.
+  await flutterLocalNotificationsPlugin
+      .resolvePlatformSpecificImplementation<
+          AndroidFlutterLocalNotificationsPlugin>()
+      ?.createNotificationChannel(channel);
+
+  /// Update the iOS foreground notification presentation options to allow
+  /// heads up notifications.
+  await FirebaseMessaging.instance.setForegroundNotificationPresentationOptions(
+    alert: true,
+    badge: true,
+    sound: true,
+  );
+  isFlutterLocalNotificationsInitialized = true;
+}
+
+void showFlutterNotification(RemoteMessage message) {
+  RemoteNotification? notification = message.notification;
+  AndroidNotification? android = message.notification?.android;
+  if (notification != null && android != null && !kIsWeb) {
+    debugPrint(notification.title);
+    flutterLocalNotificationsPlugin.show(
+      notification.hashCode,
+      notification.title,
+      notification.body,
+      NotificationDetails(
+        android: AndroidNotificationDetails(
+          channel.id,
+          channel.name,
+          channelDescription: channel.description,
+          // TODO add a proper drawable resource to android, for now using
+          //      one that already exists in example app.
+          icon: 'launch_background',
+        ),
+      ),
+    );
+  }
+}
+
+/// Initialize the [FlutterLocalNotificationsPlugin] package.
+late FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin;
 
 // 앱에서 지원하는 언어 리스트 변수
 final supportedLocales = [const Locale('en', 'US'), const Locale('ko', 'KR')];
@@ -18,6 +93,15 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   //Firebase 초기화
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  //알림 수신을 위한 권한 요청
+  if (await Permission.notification.isDenied) {
+    await Permission.notification.request();
+  }
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+  if (!kIsWeb) {
+    await setupFlutterNotifications();
+  }
+
   // easylocalization 초기화!
   await EasyLocalization.ensureInitialized();
   //shared_preferences 초기화
@@ -61,14 +145,9 @@ void main() async {
   );
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
   Future chkLogIn() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     final String? loginToken = prefs.getString('login_token');
@@ -96,15 +175,27 @@ class _MyAppState extends State<MyApp> {
       locale: context.locale,
       title: 'Sinokor App',
       theme: Provider.of<ThemeProvider>(context).themeData,
-      home: FutureBuilder(
-        future: Future.delayed(const Duration(seconds: 3), chkLogIn),
-        builder: (context, snapshot) {
-          return AnimatedSwitcher(
-            duration: const Duration(microseconds: 1000),
-            child: _splashLoadingWidget(snapshot),
-          );
-        },
-      ),
+      routes: {
+        '/': (context) => FutureBuilder(
+              future: Future.delayed(const Duration(seconds: 3), chkLogIn),
+              builder: (context, snapshot) {
+                return AnimatedSwitcher(
+                  duration: const Duration(microseconds: 1000),
+                  child: _splashLoadingWidget(snapshot),
+                );
+              },
+            ),
+        '/message': (context) => const MessageView(),
+      },
+      // home: FutureBuilder(
+      //   future: Future.delayed(const Duration(seconds: 3), chkLogIn),
+      //   builder: (context, snapshot) {
+      //     return AnimatedSwitcher(
+      //       duration: const Duration(microseconds: 1000),
+      //       child: _splashLoadingWidget(snapshot),
+      //     );
+      //   },
+      // ),
     );
   }
 }
@@ -118,5 +209,55 @@ Widget _splashLoadingWidget(AsyncSnapshot<Object?> snapshot) {
     );
   } else {
     return const SplashScreen();
+  }
+}
+
+// Crude counter to make messages unique
+int _messageCount = 0;
+
+/// The API endpoint here accepts a raw FCM payload for demonstration purposes.
+String constructFCMPayload(String? token) {
+  _messageCount++;
+  return jsonEncode({
+    'token': token,
+    'data': {
+      'via': 'FlutterFire Cloud Messaging!!!',
+      'count': _messageCount.toString(),
+    },
+    'notification': {
+      'title': 'Hello FlutterFire!',
+      'body': 'This notification (#$_messageCount) was created via FCM!',
+    },
+  });
+}
+
+/// UI Widget for displaying metadata.
+class MetaCard extends StatelessWidget {
+  final String _title;
+  final Widget _children;
+
+  // ignore: public_member_api_docs
+  const MetaCard(this._title, this._children, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      margin: const EdgeInsets.only(left: 8, right: 8, top: 8),
+      child: Card(
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            children: [
+              Container(
+                margin: const EdgeInsets.only(bottom: 16),
+                child: Text(_title, style: const TextStyle(fontSize: 18)),
+              ),
+              _children,
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
